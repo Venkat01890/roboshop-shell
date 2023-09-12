f597f13c208465e98ae128433be67ce87f260f34
Venkat01890 <veera.srikakolapu18@gmail.com>
2023-09-12 13:39:53 +0530

    added server scripts

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-01748e1a5257a1e58
+
 
 for i in "${NAMES[@]}"
 do
+    INSTANCE_TYPE="t2.micro"
     echo "NAME: $i"
+    echo "creating $i instance"
+    $ aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids 
+    $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
