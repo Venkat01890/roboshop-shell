97e6f91ce2700e334d2eac23ee40b53c7a6b2ae2
Venkat01890 <veera.srikakolapu18@gmail.com>
2023-09-12 14:24:44 +0530

    added server scripts

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,8 +11,6 @@ do
     INSTANCE_TYPE="t2.micro"
     echo "NAME: $i"
     echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids 
-    $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].
-    PrivateIpAddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "Created $i instance : $IP_ADDRESS"
 done
